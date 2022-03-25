89eead8eba154fd25ccf53a452ac520a354f7c4e
BrunoLoFaro <lofarobruno@gmail.com>
2022-03-25 17:30:24 -0300

    Emailing

--- Vidly/Controllers/API/OrderController.cs
+++ Vidly/Controllers/API/OrderController.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using AutoMapper;
 using Microsoft.AspNet.Identity;
 using Newtonsoft.Json;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using Vidly.Dtos;
 using Vidly.DTOs;
 using Vidly.Models;
@@ -41,7 +45,7 @@ namespace Vidly.Controllers.API
                 //Define request data format  
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                client.Timeout = TimeSpan.FromMilliseconds(100);
+                //client.Timeout = TimeSpan.FromMilliseconds(100);
 
                 //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
                 PaymentResponse response;
@@ -72,8 +76,32 @@ namespace Vidly.Controllers.API
         [HttpPost]
         public IHttpActionResult CreateOrder()
         {
-            Task.Run(myFunc);
+
+            //Task.Run(myFunc);
+
+            Execute().Wait();
             return Ok();
+
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("EMAIL_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("lofarobruno@gmail.com", "Sending User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("lofarobruno@gmail.com", "Receiving User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            try
+            {
+                var response = await client.SendEmailAsync(msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
         /* [HttpPost]
          public IHttpActionResult CreateOrder(OrderDto orderDto)
